9c116dda154b823d6e294f592b656b5e2c540aae
Brandon Auyeung <bauyeung@tuta.com>
2026-01-30 18:16:32 -0800

    Added 'Given' files.

---
+++ ce443543w26hw4/calc_spectra.m
@@ -0,0 +1,13 @@
+clear all
+close all
+clc
+
+time = xlsread('Centrifuge_acc_NJM02.xlsx','Acc','A3:A2002');
+acc = xlsread('Centrifuge_acc_NJM02.xlsx','Acc','C3:C2002');
+
+periods = [logspace(log10(0.01),log10(10),300)]';
+damping = 0.05;
+
+psa = rspec(time,acc,periods,damping);
+
+
