4b8321c84fbd6859a01c045feac76567e4bcf425
Brandon Auyeung <bauyeung@tuta.com>
2026-02-04 21:56:01 -0800

    Spectra for Problem 2 obtained

--- ce443543w26hw4/calc_spectra.m
+++ ce443543w26hw4/calc_spectra.m
@@ -3,11 +3,9 @@ close all
 clc
 
 time = xlsread('Centrifuge_acc_NJM02.xlsx','Acc','A3:A2002');
-acc = xlsread('Centrifuge_acc_NJM02.xlsx','Acc','C3:C2002');
+acc = xlsread('Centrifuge_acc_NJM02.xlsx','Acc','H3:H2002');
 
 periods = [logspace(log10(0.01),log10(10),300)]';
 damping = 0.05;
 
 psa = rspec(time,acc,periods,damping);
-
-
