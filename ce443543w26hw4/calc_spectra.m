5f5df3009e7f9b1d40197518bd97b60e25a437e9
Brandon Auyeung <bauyeung@tuta.com>
2026-02-06 14:41:39 -0800

    saving work.

--- ce443543w26hw4/calc_spectra.m
+++ ce443543w26hw4/calc_spectra.m
@@ -3,7 +3,7 @@ close all
 clc
 
 time = xlsread('Centrifuge_acc_NJM02.xlsx','Acc','A3:A2002');
-acc = xlsread('Centrifuge_acc_NJM02.xlsx','Acc','H3:H2002');
+acc = xlsread('Centrifuge_acc_NJM02.xlsx','Acc','B3:B2002');
 
 periods = [logspace(log10(0.01),log10(10),300)]';
 damping = 0.05;
